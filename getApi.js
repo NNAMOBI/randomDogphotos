1caacff2da1222e42937cccc6455b0bdc7dc979f
Nnamdi Osuagwu <kelvinosuagwu@Nnamdis-MacBook-Air.local>
2020-03-23 04:56:50 +0100

    i designed a UI that generated random dog pictures at the click of a button that says "get random dog"

--- getApi.js
+++ getApi.js
@@ -1,6 +1,9 @@
 let button = document.querySelector('button')
+let img = document.querySelector('#dogImage')
 
-console.log(button)
+// console.log(img)
+
+// console.log(button)
 
 button.addEventListener('click', function(){
     console.log('i have just clicked a button')
@@ -9,9 +12,15 @@ button.addEventListener('click', function(){
 
     xhttp.onreadystatechange = function (){
         if(this.readyState === 4 && this.status === 200){
-            console.log( this.responseText.message)
+            let dataParsed = JSON.parse(this.responseText)
+            console.log( dataParsed.message)
+            let url = dataParsed.message
             // console.log(this.responseText)
-            console.log(typeof this.responseText)
+            // console.log(typeof this.responseText) // This is a string therefor it is seeing the json as a string 
+                                                  // therefore we have to parse the json to normal object literal
+
+             img.src = url;
+             console.log(img.src)                                     
         }
     // else {
     //     console.log('not available' + this.status)
